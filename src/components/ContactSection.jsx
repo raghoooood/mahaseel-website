b80a23e8a911ca3ddb243197b9164ab3d2b6fafe
raghoooood <raghad.nakshou@gmail.com>
2025-02-04 18:53:19 +0300

    h2

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -9,12 +9,14 @@ const ContactSection = () => {
     <Container className="mt-24 sm:mt-32 lg:mt-40">
       <FadeIn className="rounded-4xl bg-gradient-to-r from-sky-500 to-sky-300 p-8 sm:p-12 md:p-16 shadow-xl">
         <div className="max-w-6xl mx-auto text-center">
-          <h2 className="font-display text-4xl font-bold text-white sm:text-5xl mb-6">
-            We'd Love to Hear From You!
-          </h2>
-          <p className="text-lg text-white opacity-80 mb-8">
-            Have any questions or want to work with us? Don't hesitate to reach out!
-          </p>
+        <h2 className="font-display text-4xl font-bold text-white sm:text-5xl mb-6">
+  We&apos;d Love to Hear From You!
+</h2>
+
+<p className="text-lg text-white opacity-80 mb-8">
+  Have any questions or want to work with us? Don&apos;t hesitate to reach out!
+</p>
+
           <div className="flex justify-center mb-10">
             <Button href="/contact" className="px-8 py-3 text-lg font-semibold  text-sky-500 hover:bg-sky-500 hover:text-white transition duration-300 rounded-full shadow-lg">
               Say Hello
